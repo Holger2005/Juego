5e3da260666a41e3b11f362eeef6da259ce1688b
Holger2005 <ordonezholger43@gmail.com>
2026-09-16 02:06:46 -0500

    feat: implementar sistema de animaciones 2D y logica de movimiento del jugador

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,4 +1,8 @@
+using System.Numerics;
+using System.Reflection;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,24 +11,40 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 7f;
     public bool isGrounded;
+    private Animator animator;
+    private bool facinRigth = true;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+// Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rd = GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float move = Input.GetAxis("Horizontal");
-        rd.velocity = new Vector2(move * moveSpeed, rd.velocity.y);
+        float speedAnimation = Mathf.Abs(move);
+        animator.SetFloat("Speed", speedAnimation);
+  
+
+        rd.linearVelocity = new Vector2(move * moveSpeed, rd.linearVelocity.y);
 
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rd.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
             isGrounded = false;
+            animator.SetBool("isJump",true);
+        }
+
+        if(move > 0 && !facinRigth){
+            Flip();
+        }else if (move < 0 && facinRigth){
+            Flip(); 
         }
     }
 
@@ -33,14 +53,23 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            animator.SetBool("isJump", false);
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+     void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
+
         }
+    }
+
+    void Flip(){
+        facinRigth = !facinRigth;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
